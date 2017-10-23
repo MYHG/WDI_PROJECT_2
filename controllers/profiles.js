f5f6d0fe9c66f43b1dd48e8b7ddab522edd67720
SandraOkoli <sandraokoli@hotmail.co.uk>
2017-10-23 13:41:06 +0100

    Exported profiles controller

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -11,6 +11,9 @@ function indexRoute (req, res, next) {
     .catch(next);
 }
 //NEW
+function newRoute(req, res) {
+  return res.render('profiles/new')
+}
 
 //SHOW
 function showRoute (req, res, next) {
@@ -22,4 +25,73 @@ function showRoute (req, res, next) {
      return res.render('profiles/show', { profile });
    })
    .catch(next);
+}
+
+//CREATE
+function createRoute(req, res, next) {
+  Profile
+   .create(req.body)
+   .then(() => res.redirect('/profiles'))
+   .catch((err) => {
+     if (err.name === 'ValidationError') return res.badRequest(`/profiles/${req.params.id}/edit`, err.toString()
+     next(err)
+   });
+}
+//EDIT
+function editRoute(req, res, next) {
+  Profile
+  .findById(req.params.id)
+  .exec()
+  .then(profile) => {
+    if(!profile) return res.notFound();
+    if(!profile.belongsTo(req.user)) return res.unauthorized('You do not have access rights')
+    return res.render{'profiles/edit', {profile}}
+  })
+.catch(next);
+}
+
+//UPDATE
+function updateRoute(req, res, next) {
+  Profile
+   .findById(req.params.id)
+   .exec()
+   .then(profile) => {
+     if(!profile) return res.notFound();
+     if(!profile.belongsTo(req.user)) return res.unauthorized('You do not have access rights')
+
+     for(const field in req.body){
+       profile[field] = req.body[field];
+     }
+     return profile.save();
+   })
+   .then(() => res.redirect('/profiles'))
+   .catch((err) => {
+     if (err.name === 'ValidationError') return res.badRequest(`/profiles/${req.params.id}/edit`, err.toString()
+     next(err)
+   });
+}
+
+//DELETE
+function deleteRoute(req, res, next) {
+  Profile
+   .findById(req.params.id)
+   .exec()
+   .then(profile => {
+     if(!profile) return res.notFound();
+     if(!profile.belongsTo(req.user)) return res.unauthorized('You do not have access rights')
+     return profile.remove();
+   })
+   .then(() => res.redirect('/profiles'))
+   .catch(next);
+}
+
+//Export all routes
+module.exports = {
+  index: indexRoute,
+  new: newRoute,
+  show: showRoute,
+  create: createRoute,
+  edit: editRoute,
+  update: updateRoute,
+  delete: deleteRoute
 }
--- models/profile.js
+++ models/profile.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 
+const quoteSchema = new mongoose.Schema({
+  content: {type: String, required: true }
+});
 const yearbookSchema = new mongoose.Schema({
   name: {type: String, required: true},
   image: {type: String, required: true},
-  quote: {type: String, required: true}
+  quote: [ quoteSchema ]
 });
 
-yearbookSchema.methods.belongsTo = function profiileBelongsTo(user) {
-  if(typeof this.createdBy.id === 'string') return this.createdBy.id === user.id;
-  return user.id === this.createdBy.toString();
-};
+// yearbookSchema.methods.belongsTo = function profiileBelongsTo(user) {
+//   if(typeof this.createdBy.id === 'string') return this.createdBy.id === user.id;
+//   return user.id === this.createdBy.toString();
+// };
 
 module.exports = mongoose.model('Profile', yearbookSchema);
