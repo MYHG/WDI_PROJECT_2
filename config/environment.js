412f52f3003ce932834f39383a9992ad692d4a9d
SandraOkoli <sandraokoli@hotmail.co.uk>
2017-10-26 09:50:52 +0100

    deployed

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  databaseURL: process.env.MONGOB_URI || 'mongodb://localhost/all-the-birds',
+  databaseURL: process.env.MONGODB_URI || 'mongodb://localhost/all-the-birds',
   sessionSecret: process.env.SESSION_SECRET || 'YghT5s617/1{%sDt'
 };
